1c78a6efe7d5c0df804719671ed20bd19d76f606
David Matter <david.matter@me.com>
2021-04-14 11:59:26 +0200

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,29 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    {{ a }}
+    {{ b.c }}
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { computed, defineComponent, ref } from "@vue/composition-api";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-export default Vue.extend({
+export default defineComponent({
   name: "Home",
   components: {
     HelloWorld,
   },
+  setup() {
+    return {
+      a: ref(3),
+      b: computed(() => {
+        return {
+          c: 2
+        }
+      })
+    }
+  }
 });
 </script>
